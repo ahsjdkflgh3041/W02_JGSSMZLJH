081d0b49a00006d83bea2bbe3b3f9ffee0d9f3ac
손명진 <sonmyungzin1@gmail.com>
2023-08-25 10:56:37 +0900

    (+) 특정 방향으로 약대쉬하는 기능 추가

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,22 +8,39 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D m_rigidBody;
     PlayerGround m_ground;
 
-    [SerializeField] private bool m_enableDoubleJump = true;
+    [Header("Move")]
     [SerializeField] private float m_speedX;
+    [Header("Jump")]
+    [SerializeField] private bool m_enableDoubleJump = true;
     [SerializeField] private float m_jumpHeight;
     [SerializeField] private float m_jumpTimeToApex;
-    [SerializeField] private float m_gravityMultiplier = 1;
+    [SerializeField] private float m_defaultGravity = 1;
+    [Header("Dash")]
+    [SerializeField] private float m_dashDistance;
+    [SerializeField] private float m_dashPreDelay;
+    //[SerializeField] private float m_dashTime;
+    [SerializeField] private float m_dashPostDelay;
+    [Header("Input")]
+    [SerializeField] private float m_jumpBuffer = 0.2f;
     [SerializeField] private float m_dashCriterionTime = 0.2f;
 
     private Vector2 m_velocity;
     private float m_directionX;
-    private bool m_desiredJump;
-    private bool m_desiredDash;
     private float m_desiredVelocityX;
+    private bool m_desiredJump;
+    private float m_gravityMultiplier;
+    private bool m_desiredDash;
+    // Temp serialization
+    [Header("temp serialization")] [SerializeField] private Vector2 m_dashDirection;
+    private Vector2 m_dashStartPosition;
+    private Vector2 m_dashTargetPosition;
+    private float m_dashStartTime;
+    private float m_jumpBufferCounter;
     private float m_dashInputTime;
     private bool m_dashInput;
 
     private bool m_isDashing;
+    private bool m_hasPerformedDash;
     private bool m_isJumping;
     private bool m_canJumpAgain = false;
     private bool m_onGround;
@@ -34,6 +51,8 @@ public class PlayerController : MonoBehaviour
     {
         m_rigidBody = GetComponent<Rigidbody2D>();
         m_ground = GetComponent<PlayerGround>();
+
+        m_gravityMultiplier = m_defaultGravity;
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -78,22 +97,43 @@ public class PlayerController : MonoBehaviour
     {
         m_rigidBody.gravityScale = (m_gravityMultiplier * (-2 * m_jumpHeight) / (m_jumpTimeToApex * m_jumpTimeToApex)) / Physics2D.gravity.y;
 
+        if (m_jumpBuffer > 0)
+        {
+            if (m_desiredJump)
+            {
+                m_jumpBufferCounter += Time.deltaTime;
+
+                if (m_jumpBufferCounter > m_jumpBuffer)
+                {
+                    m_desiredJump = false;
+                    m_jumpBufferCounter = 0;
+                }
+            }
+        }
+
         m_onGround = m_ground.GetOnGround();
         m_desiredVelocityX = (m_directionX == 0 ? 0 : Mathf.Sign(m_directionX)) * m_speedX;
     }
 
     private void FixedUpdate()
     {
-        m_velocity = m_rigidBody.velocity;
-        m_velocity.x = m_desiredVelocityX;
-
-        if (m_desiredJump)
+        if (m_isDashing)
         {
-            DoAJump();
+            PerformDash();
         }
-        if (m_desiredDash)
+        else
         {
-            DoADash();
+            m_velocity = m_rigidBody.velocity;
+            m_velocity.x = m_desiredVelocityX;
+
+            if (m_desiredDash)
+            {
+                DoADash();
+            }
+            else if (m_desiredJump)
+            {
+                DoAJump();
+            }
         }
 
         m_rigidBody.velocity = m_velocity;
@@ -102,15 +142,50 @@ public class PlayerController : MonoBehaviour
         {
             m_isJumping = false;
         }
+
+        SetGravity();
     }
 
+    private void PerformDash()
+    {
+        float dashTime = Time.time - m_dashStartTime;
+        m_velocity = Vector2.zero;
+
+        if (dashTime <= m_dashPreDelay)
+        {
+
+        }
+        //else if (dashTime <= m_dashPreDelay + m_dashTime)
+        //{
+        //    //m_velocity = m_dashDirection.normalized * m_dashDistance / m_dashTime;
+
+        //    m_velocity = Vector2.zero;
+        //    transform.position = Vector2.Lerp(m_dashStartPosition, m_dashTargetPosition, dashTime - m_dashPreDelay);
+        //}
+        else if (dashTime <= m_dashPreDelay + m_dashPostDelay)
+        {
+            if (!m_hasPerformedDash)
+            {
+                m_hasPerformedDash = true;
+                transform.Translate(m_dashDirection.normalized * m_dashDistance, Space.Self);
+            }
+        }
+        else
+        {
+            var displacement = (Vector2)transform.position - m_dashStartPosition;
+            Debug.Log($"Dash Displacement : {displacement} = {displacement.magnitude}");
+            m_isDashing = false;
+        }
+    }
     private void DoAJump()
     {
         if (m_onGround || m_canJumpAgain)
-        { 
-            float jumpSpeed = Mathf.Sqrt(-2f * Physics2D.gravity.y * m_rigidBody.gravityScale * m_jumpHeight);
-
+        {
+            m_desiredJump = false;
+            m_jumpBufferCounter = 0;
             m_canJumpAgain = m_canJumpAgain == false;
+
+            float jumpSpeed = Mathf.Sqrt(-2f * Physics2D.gravity.y * m_rigidBody.gravityScale * m_jumpHeight);
 
             if (m_velocity.y > 0f)
             {
@@ -125,7 +200,6 @@ public class PlayerController : MonoBehaviour
             m_isJumping = true;
         }
 
-        m_desiredJump = false;
     }
 
     private void DoADash()
@@ -133,5 +207,15 @@ public class PlayerController : MonoBehaviour
         Debug.Log("Dash!");
         m_isDashing = true;
         m_desiredDash = false;
+        m_hasPerformedDash = false;
+
+        m_dashStartPosition = (Vector2)transform.position;
+        m_dashTargetPosition = (Vector2)transform.position + m_dashDirection.normalized * m_dashDistance;
+        m_dashStartTime = Time.time;
+    }
+
+    private void SetGravity()
+    {
+        m_gravityMultiplier = m_isDashing ? 0 : m_defaultGravity;
     }
 }
