8ca8902ebdbbea586e03e63300675798512de4c1
LeeJiHun <141272067+2Irada@users.noreply.github.com>
2023-08-28 21:52:00 +0900

    [+] Intro,Ending,BuildSetting Add
    인트로, 엔딩 및 Scene 이동 추가

--- Assets/Scripts/Stage/GoNextLevel.cs
+++ Assets/Scripts/Stage/GoNextLevel.cs
@@ -10,6 +10,20 @@ public class GoNextLevel : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void GoStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void gameQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif       
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
